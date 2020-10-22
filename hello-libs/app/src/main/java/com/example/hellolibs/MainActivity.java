0a9e864325780ca3b1426728d0bd38a3d4f0dba6
jiaquan11 <913882743@qq.com>
2020-10-22 18:37:41 +0800

    *ndk-samples: hello-libs项目学习调试

--- hello-libs/app/src/main/java/com/example/hellolibs/MainActivity.java
+++ hello-libs/app/src/main/java/com/example/hellolibs/MainActivity.java
@@ -15,25 +15,26 @@
  */
 package com.example.hellolibs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
 /*
  * Simple Java UI to trigger jni function. It is exactly same as Java code
  * in hello-jni.
  */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
-        tv.setText( stringFromJNI() );
+        tv.setText(stringFromJNI());
         setContentView(tv);
     }
-    public native String  stringFromJNI();
+
+    public native String stringFromJNI();
+
     static {
         System.loadLibrary("hello-libs");
     }
-
 }
