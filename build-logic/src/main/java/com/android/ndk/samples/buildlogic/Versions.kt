c9da9f7bc01de9a35af8bfb15c7c7ff41c020b2c
jiaquan11 <913882743@qq.com>
2026-05-03 20:32:02 +0800

    ndk-samples:1、修改配置文件，保证工程正常编译和运行

--- build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
+++ build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
@@ -3,10 +3,10 @@ package com.android.ndk.samples.buildlogic
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val COMPILE_SDK = 35
+    const val COMPILE_SDK = 36
     const val TARGET_SDK = 35
     const val MIN_SDK = 21
     const val NDK = "29.0.14206865" // r29
-    const val CMAKE = "4.1.0"
+    const val CMAKE = "4.1.2"
     val JAVA = JavaVersion.VERSION_1_8
 }
